96dea6c4a469675c800c0f3b41dd13fc06604627
Jack Leighton <jakka351@outlook.com>
2025-02-13 04:32:49 -0500

    Add files via upload

---
+++ Properties/Resources.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace FG2ICCComms.Properties
+{
+	// Token: 0x02000008 RID: 8
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
+	internal class Resources
+	{
+		// Token: 0x06000035 RID: 53 RVA: 0x00004712 File Offset: 0x00002912
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000005 RID: 5
+		// (get) Token: 0x06000036 RID: 54 RVA: 0x0000471C File Offset: 0x0000291C
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (object.ReferenceEquals(Resources.resourceMan, null))
+				{
+					ResourceManager resourceManager = new ResourceManager("FG2ICCComms.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = resourceManager;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000006 RID: 6
+		// (get) Token: 0x06000037 RID: 55 RVA: 0x0000475B File Offset: 0x0000295B
+		// (set) Token: 0x06000038 RID: 56 RVA: 0x00004762 File Offset: 0x00002962
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x04000028 RID: 40
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000029 RID: 41
+		private static CultureInfo resourceCulture;
+	}
+}
